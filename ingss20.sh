239c87cb282ae4c7b06baea6396ae3e91ab7c3ce
Mr-IngsS <54253110+Mr-IngsS@users.noreply.github.com>
2019-11-06 00:17:19 +0700

    Vakum

---
+++ ingss20.sh
@@ -0,0 +1,179 @@
+#!system/xbin/bash
+clear
+echo "\033[0;33m                            _________"
+echo "\033[0;33m         _________          |       |"
+echo "\033[0;33m         |       |          | • • • |"
+echo "\033[0;33m________ | • • • | ________ | • • • |"
+echo "\033[0;33m|      | | • • • | |      | | • • • |"
+echo "\033[0;33m|  • • | | • • • | | • •  | | • • • |"
+echo "\033[0;33m|  • • | | • • • | | • •  | | • • • |"
+echo "\033[0;33m|  • • | | • • • | | • •  | | • • • |"
+echo "\033[0;33m|  • • | | • • • | | • •  | | • • • |"
+echo "\033[0;33m|______| |_______| |______| |_______|"
+echo "\033[1;36m|=====================================|" |lolcat
+echo "\033[1;36m|   •   Author : Mr.IngsS             |" |lolcat
+echo "\033[1;36m|   •   MyTeam : BlackCyberTeam       |" |lolcat
+echo "\033[1;36m|   •   contact: mringss2@gmail       |" |lolcat
+echo "\033[1;36m|   •   contact: 089512758056         |" |lolcat
+echo "\033[1;36m|=====================================|" |lolcat
+echo "\033[1;37m   •    BERPIKIRLAH SEBAGAI PEMULA,MAKA DUNIA AKAN TERBUKA UNTUK"
+echo "\033[1;37m   •    GUNAKAN TOOLS INI DENGAN BIJAK !!"
+echo "\033[1;37m   •    KARNA KAMI TIDAK AKAN TANGGUNG JAWAB ATAS PERBUATAN KALIAN"
+echo "\033[1;37m   •    SUBSCRIBE CHANEL YAOUTUBE [> SAYDOG <]"
+echo "\033[1;31m        [ WE ARE BLACKCYBERTEAM ]"
+echo "\033[1;31m[ WE ARE LEGION ]"
+echo "\033[1;31m        [ WE DO NOT FORGIVE ]"
+echo "\033[1;31m[ WE DO NOT FORGET ]"
+echo "\033[1;31m        [ ESPECT US ]"
+echo "\033[0;32m|==================MENU-TOOLS====================|"
+echo "\033[0;32m|                                                |"
+echo "\033[0;32m|          ¤ SILAHKAN PILIH TOOLS NYA ¤          |"
+echo "\033[0;32m|                                                |"
+echo "\033[0;32m| •     {1}. HACKED FACEBOOK =>                  |"
+echo "\033[0;32m|   •   {2}. BTUTEFORCE FACEBOOK =>              |"
+echo "\033[0;32m| •     {3}. CREATE VIRUS ANDROID =>             |"
+echo "\033[0;32m|   •   {4}. METASPLOIT-HELPER =>                |"
+echo "\033[0;32m| •     {5}. NEWSX INSTALLER =>                  |"
+echo "\033[0;32m| •     {6}. EVIL CREATE FRAMEWORK =>            |"
+echo "\033[0;32m|   •   {7}. SAYDOG =>                           |"
+echo "\033[0;32m| •     {8}. DARK-SMS WORK =>                    |"
+echo "\033[0;32m|   •   {9}.TERKEY =>                            |"
+echo "\033[0;32m| •     {10}.INFO TEAM =>                        |"
+echo "\033[0;32m|   •   {11}.CLOSE/EXIT TOOLS =>                 |"
+echo "\033[0;32m|                                                |"
+echo "\033[0;32m|  TERIMA KASIH SUDAH MENGGUNKAN TOOLS KAMI :)   |"
+echo "\033[0;32m|================================================|"
+read -p "PILIH TOOLS ANDA :" pilih
+if [ $pilih = "1" ]
+then
+echo "Installing HACKED FACEBOOK....."
+cd $HOME
+pkg install python2
+pkg install git
+git clone https://github.com/bukanhacker/Sad
+cd Sad
+python2 Sad.py
+fi
+
+if [ $pilih = "2" ]
+then
+echo "Installing BruteForce Fb....."
+cd $HOME
+apt install python2
+pkg install pip2
+pip2 install bs4
+pip2 install requests
+git clone https://github.com/FR13ND8/BruteFb.git
+cd BruteFb
+python2 brute.py
+fi
+
+if [ $pilih = "3" ]
+then
+echo "Installing Create Virus Android....."
+cd $HOME
+pkg install git wget curl
+pkg install python2
+git clone https://github.com/saydog/Virus-droid-creator.git
+cd Virus-droid-creator
+python2 dog.py
+fi
+
+if [ $pilih = "4" ]
+then
+echo "Installing Metasploit-Helper....."
+cd $HOME
+pkg install git
+pkg install python2
+git clone https://github.com/saydog/Metasploit-Helper.git
+cd Metasploit-Helper
+sh install.sh
+fi
+
+if [ $pilih = "5" ]
+then
+echo "Installing NewsX Installer....."
+cd $HOME
+pkg update && pkg upgrade
+pkg install git
+git clone https://github.com/pengangguranmuda/NewsX.git
+cd NewsX
+sh newsx.sh
+fi
+
+if [ $pilih = "6" ]
+then
+echo "Msf-Helper....."
+cd $HOME
+pkg install python2
+pkg install git
+git clone https://github.com/LOoLzeC/Evil-create-framework.git
+d Evil-create-framework
+sh install.sh
+fi
+
+if [ $pilih = "7" ]
+then
+echo "Installing Saydog....."
+cd $HOME
+pkg update && pkg upgrade
+pkg install pip2
+pkg install git
+pip2 install colorama
+git clone https://github.com/saydog/SAYDOG.git
+cd SAYDOG
+sh install.sh
+fi
+
+if [ $pilih = "8" ]
+then
+echo "Installing Dark-sms work....."
+cd $HOME
+pkg install git
+pkg install python
+pkg install pip2
+pip2 install requests
+git clone https://github.com/AchmadMaftuchinSyauqoni/SmsGratis.git
+cd SmsGratis
+python SmsGratis.pyc
+fi
+
+if [ $pilih = "9" ]
+then
+echo "Installing Terkey....."
+cd $HOME
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/karjok/terkey.git
+cd terkey
+python2 terkey.py
+fi
+
+if [ $pilih = "10" ]
+then
+echo "VISIT INFO TEAM....."
+echo "\033[0;32m|===========We Are BlackCyberTeam============|"
+echo "\033[0;32m|                                            |"
+echo "\033[0;32m|    JIKA MENGHINA KAMI KALIAN SUDAH HOBI    |"
+echo "\033[0;32m|       MAKA DALAM BIDANG BODOAMAT LAH       |"
+echo "\033[0;32m|       KAMI SUDAH MENDADI AHLI              |"
+echo "\033[0;32m|                                            |"
+echo "\033[0;32m|         WE ARE BLACKCYBERTEAM...           |"
+echo "\033[0;32m|                                            |"
+echo "\033[0;32m|       • Mr.IngsS                           |"
+echo "\033[0;32m|       • Mr.ClayBot                         |"
+echo "\033[0;32m|       • Mr.H4CKYOU7H                       |"
+echo "\033[0;32m|       • Mr.J4NC0K                          |"
+echo "\033[0;32m|       ° Mr.Style                           |"
+echo "\033[0;32m|       • Mr.B17CH                           |"
+echo "\033[0;32m|                                            |"
+echo "\033[0;32m|============================================|"
+fi
+
+if [ $pilih = "11" ]
+then
+echo "TERIMA KASIH SUDAH MENGGUNAKAN TOOLS KAMI"
+echo "TOLONG HARGAI PEMBUAT SCRIPT INI"
+        exit
+fi
